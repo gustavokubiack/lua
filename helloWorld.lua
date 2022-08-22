e05eecb58c1e2cb200a7863720c07d2794ef5b5a
gustavokubiack <guukubiack@gmail.com>
2022-08-21 22:54:52 -0300

    first commit

---
+++ helloWorld.lua
@@ -0,0 +1,13 @@
+print('hello world')
+
+a = 5
+b = 10
+soma = a + b
+print(soma)
+
+myName = 'Gustavo'
+age = 18
+hobby = 'programming'
+
+print('My name is ' .. myName .. ' and I am ' ..age.. ' years old. My favorite hobby is ' ..hobby ..'.')
+
