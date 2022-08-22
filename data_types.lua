4bff1e79e36500690470742d20a0f804e5821ef7
gustavokubiack <guukubiack@gmail.com>
2022-08-22 09:13:38 -0300

    renamed files and data_typed added

---
+++ data_types.lua
@@ -0,0 +1,12 @@
+num1 = 5
+print(type(num1)) -- number
+
+num2 = 10.5
+print(type(num2)) -- number too
+
+name = "Gustavo Kubiack"
+print(type(name)) -- string
+
+color = false
+print(type(color)) -- boolean
+
